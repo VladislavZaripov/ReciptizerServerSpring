20e142b1fdb2723745f8e15f1f0aaa7debd74db8
VladislavZaripov <zaripovva@mail.ru>
2020-08-07 13:14:33 +0300

    commit

--- src/main/java/com/zaripov/reciptizer/server/controller/AppRestController.java
+++ src/main/java/com/zaripov/reciptizer/server/controller/AppRestController.java
@@ -1,14 +1,14 @@
 package com.zaripov.reciptizer.server.controller;
 
+import com.zaripov.reciptizer.server.service.ImageService;
 import com.zaripov.reciptizer.server.entity.Recipe;
 import com.zaripov.reciptizer.server.entity.RecipeFilter;
 import com.zaripov.reciptizer.server.service.RecipeFilterService;
 import com.zaripov.reciptizer.server.service.RecipeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.imageio.ImageIO;
+
 import javax.servlet.http.HttpServletRequest;
-import java.awt.image.BufferedImage;
 import java.io.*;
 
 @RestController
@@ -18,61 +18,46 @@ public class AppRestController {
 
     private final RecipeService recipeService;
     private final RecipeFilterService recipeFilterService;
+    private final ImageService imageService;
 
-    public AppRestController(RecipeService recipeService, RecipeFilterService recipeFilterService) {
+    public AppRestController(RecipeService recipeService, RecipeFilterService recipeFilterService,ImageService imageService) {
         this.recipeService = recipeService;
         this.recipeFilterService = recipeFilterService;
+        this.imageService = imageService;
     }
 
     @GetMapping("getRecipe/{id}")
-    @ResponseBody
     public Recipe getRecipe(@PathVariable String id){
         return recipeService.getRecipe(Integer.parseInt(id));
     }
 
     @GetMapping("getFilter")
-    @ResponseBody
     public RecipeFilter getFilter(){
         return recipeFilterService.getRecipeFilter();
     }
 
     @GetMapping("getImage/{imageName}")
-    @ResponseBody
     public byte[] getImage(@PathVariable("imageName") String imageName){
-        File file = new File("C://Users/zarip/Desktop/ImageSpring/" + imageName);
-        byte[] imageInByte;
         try {
-            BufferedImage image = ImageIO.read(file);
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(image, "png", baos);
-            baos.flush();
-            imageInByte = baos.toByteArray();
-            baos.close();
+            return imageService.loadImage(imageName);
         } catch (IOException e) {
             return null;
         }
-        return imageInByte;
     }
 
     @PostMapping ("postRecipe")
     public ResponseEntity postRecipe(@RequestBody Recipe recipe){
         recipeService.saveRecipe(recipe);
-        return ResponseEntity.ok().body("Recipe saved");
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("postImage/{imageName}")
-    public ResponseEntity postImage(@PathVariable("imageName") String imageName, HttpServletRequest request) throws IOException {
-        File file = new File("C://Users/zarip/Desktop/ImageSpring/" + imageName);
-        InputStream inputStream = request.getInputStream();
+    public ResponseEntity postImage(@PathVariable("imageName") String imageName, HttpServletRequest request){
         try {
-            BufferedImage image = ImageIO.read(inputStream);
-            OutputStream outputStream = new FileOutputStream(file);
-            ImageIO.write(image,"png",outputStream);
-            inputStream.close();
-            outputStream.close();
+            imageService.saveImage(imageName,request);
+            return ResponseEntity.ok().build();
         } catch (IOException e) {
-            ResponseEntity.badRequest().body("Image not saved");
+            return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok().body("Image saved");
     }
 }
--- src/main/java/com/zaripov/reciptizer/server/entity/Recipe.java
+++ src/main/java/com/zaripov/reciptizer/server/entity/Recipe.java
@@ -3,6 +3,7 @@ package com.zaripov.reciptizer.server.entity;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,4 +12,5 @@ public class Recipe {
     public Table1 table1;
     public List<Table2Row> rowsTable2;
     public List <Table3Row> rowsTable3;
+
 }
